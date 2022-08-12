0ffc29472c32674db7f008fa83564000db88c065
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-08-12 11:26:27 +0400

    commit

--- app.js
+++ app.js
@@ -3,24 +3,6 @@ const path = require ('path');
 const bodyParser=require('body-parser');//added bodyparser 
 const cors = require('cors');
 
-// const nav= [
-//     {
-//         link:"/books",
-//         title:"Books"
-//     },
-//     {
-//         link:"/authors",
-//         title:"Authors"
-//     },
-//     {
-//         link:"/addbook",
-//         title:"Add Book"
-//     },
-//     {
-//         link:"/addauthor",
-//         title:"Add Author"
-//     }
-// ]
 
 const loginRouter = require('./src/routes/loginroute');
 const signupRouter = require('./src/routes/signuproute');
