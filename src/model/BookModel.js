2886c56602e2d9183e926fd0919f62c806946be6
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-08-12 11:44:22 +0400

    commit

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+//mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect('mongodb://mongodb+srv://ajana:<I1wNLanidBwGSWxk>@cluster0.qh8z9se.mongodb.net/?retryWrites=true&w=majority/Library');
+
 const Schema = mongoose.Schema;
 
 
